71fe5d1b20f15f9b0c4ffcdf935018800489c709
4aac <management.asher@gmail.com>
2025-08-15 18:10:14 +0200

    initialized spring boot project

---
+++ src/test/java/com/onemoreproject/onemoreproject/OnemoreprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onemoreproject.onemoreproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnemoreprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
